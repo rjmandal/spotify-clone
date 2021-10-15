3793ce7d9926e718156ae42772f5936edf6b268a
rjsanjaymandal <87694411+rjsanjaymandal@users.noreply.github.com>
2021-10-16 00:18:20 +0530

    1.1

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById("masterPlay");
 let masterSongName = document.getElementById("masterSongName");
 let myProgressBar = document.getElementById("myProgressBar");
-let cover = document.getElementById("cover");
 let songItems = Array.from(document.getElementsByClassName("songItem"));
 let songs = [
     {
@@ -28,20 +27,33 @@ let songs = [
         coverPath: "cover/4.jpg"
     },
     {
-        songName: "there is nothing holding me back - Shawn Mendes",
+        songName: "there is nothing holding me back",
         filePath: "songs/5.mp3",
         coverPath: "cover/5.jpg"
-    }
+    },
+    {
+        songName: "Liar - Sandra",
+        filePath: "songs/6.mp3",
+        coverPath: "cover/1.jpg"
+    },
+    {
+        songName: "lavitating - Dua lipa",
+        filePath: "songs/7.mp3",
+        coverPath: "cover/1.jpg"
+    },
+    {
+        songName: "Treat you better -Shawn Mendes",
+        filePath: "songs/8.mp3",
+        coverPath: "cover/1.jpg"
+    },
 ]
-
+// *******************************************************************************************
 songItems.forEach((element,i)=>{
     element.getElementsByTagName("img")[0].src=songs[i].coverPath;
     element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
 })
 
-// audioElement.play();
-
-//******** */ handle play pause click****************************************
+//******** */ handle play pause click for bottom ****************************************
 masterPlay.addEventListener("click", () => {
     if (audioElement.paused || audioElement.currentTime <= 0) {
         audioElement.play();
@@ -53,25 +65,17 @@ masterPlay.addEventListener("click", () => {
         masterPlay.classList.add("fa-play-circle");
     }
 });
-// ************************************************************************
 
-// function to rotate image
-// let rotateAngle = 90;
+// update seekbar******************************************************************
 
-// function rotate(cover) {
-//     cover.setAttribute("style", "transform: rotate(" + rotateAngle + "deg)");
-//     rotateAngle = rotateAngle + 90;
-// }
-//   *********************************************
 audioElement.addEventListener("timeupdate", () => {
-    // update seekbar
     progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
     myProgressBar.value = progress;
 });
 myProgressBar.addEventListener("change", () => {
     audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
 });
-
+// ********************************************************************************
 const makeAllPlays = ()=>{
     Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
         element.classList.remove("fa-pause-circle");
@@ -82,20 +86,34 @@ const makeAllPlays = ()=>{
 Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
     element.addEventListener("click",(e)=>{
         makeAllPlays();
-        songIndex=parseInt(e.target.id);
+        if (audioElement.paused || audioElement.currentTime <= 0) {
+            songIndex=parseInt(e.target.id);
         e.target.classList.remove("fa-play-circle");
         e.target.classList.add("fa-pause-circle");
         audioElement.src=`songs/${songIndex+1}.mp3`;
         masterSongName.innerText=songs[songIndex].songName;
         audioElement.currentTime=0;
-        audioElement.play();
-        masterPlay.classList.remove("fa-play-circle");
-        masterPlay.classList.add("fa-pause-circle");
+            audioElement.play();
+            masterPlay.classList.remove("fa-play-circle");
+            masterPlay.classList.add("fa-pause-circle");
+        } 
+        else   
+         {
+            songIndex=parseInt(e.target.id);
+            e.target.classList.remove("fa-pause-circle");
+            e.target.classList.add("fa-play-circle");
+            audioElement.src=`songs/${songIndex+1}.mp3`;
+            masterSongName.innerText=songs[songIndex].songName;
+            audioElement.currentTime=0;
+            audioElement.pause();
+            masterPlay.classList.remove("fa-pause-circle");
+            masterPlay.classList.add("fa-play-circle");
+        }
     })
 })
-
+// next and previous**********************************************************
 document.getElementById("next").addEventListener('click',()=>{
-if(songIndex>=5)
+if(songIndex>=8)
 {
     songIndex=0;
 }
@@ -109,7 +127,7 @@ audioElement.play();
 masterPlay.classList.remove("fa-play-circle");
 masterPlay.classList.add("fa-pause-circle");
 });
-
+// ********************** previous
 document.getElementById("previous").addEventListener('click',()=>{
 if(songIndex<=0)
 {
@@ -125,3 +143,13 @@ audioElement.play();
 masterPlay.classList.remove("fa-play-circle");
 masterPlay.classList.add("fa-pause-circle");
 })
+// ************************************************************************
+
+// function to rotate image
+// let rotateAngle = 90;
+
+// function rotate(cover) {
+//     cover.setAttribute("style", "transform: rotate(" + rotateAngle + "deg)");
+//     rotateAngle = rotateAngle + 90;
+// }
+//   *********************************************
